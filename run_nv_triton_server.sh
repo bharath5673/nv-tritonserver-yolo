4213e7bf5c56a086f9a4f3a72a8396a91ba0919e
bharath <33729709+bharath5673@users.noreply.github.com>
2026-06-03 13:10:14 +0530

    Update Docker run command for Triton server

--- run_nv_triton_server.sh
+++ run_nv_triton_server.sh
@@ -1,7 +1,7 @@
 docker run --gpus all \
-  -v /home/bharath/Downloads/test_codes/hcl_task_32/tmp/triton_repo:/models \
+  -v tmp/triton_repo:/models \
   -p 8000:8000 \
-  nvcr.io/nvidia/tritonserver:24.09-py3 \
+  bharath5673/nv-tritonserver-yolo:24.09-py3 \
   tritonserver --model-repository=/models
 
 
